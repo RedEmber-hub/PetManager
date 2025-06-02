3c8c469397f09de401d7de3152d378c17b464d6e
RedEmber-hub <redember38@gmail.com>
2025-06-02 16:30:55 +0300

    создание экземпляра класса животного и добавление его в массив, сохранение в локалсторейдж

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const additionalFieldsDiv = document.getElementById("additional-fields");
 const animalTypeSelect = document.getElementById("animal-type");
 
+let animals = JSON.parse(localStorage.getItem("animals")) || [];
+
 animalTypeSelect.addEventListener("change", () => {
     additionalFieldsDiv.innerHTML = "";
 
@@ -28,4 +30,40 @@ animalTypeSelect.addEventListener("change", () => {
       </label>
     `;
     }
+})
+
+document.getElementById("animal-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+
+    const name = form.name.value.trim();
+    const type = form.type.value;
+    const gender = form.gender.value;
+    const birthDate = form.arrivalDate.value;
+    const city = form.city.value.trim();
+    const isSterilized = form.isSterilized.checked;
+
+    let newAnimal;
+
+    if (type === "dog") {
+        const breed = form.breed.value.trim();
+        const isTrained = form.isTrained.checked;
+        newAnimal = new Dog(name, type, gender, birthDate, city, isSterilized, breed, isTrained)
+    } else if (type === "cat") {
+        const breed = form.breed.value.trim();
+        const color = form.color.value.trim();
+        newAnimal = new Cat(name, type, gender, birthDate, city, isSterilized, breed, color);
+    } else {
+        alert("Выберите тип животного");
+        return;
+    }
+
+    animals.push(newAnimal);
+    localStorage.setItem("animals", JSON.stringify(animals));
+
+    form.reset();
+    document.getElementById("additional-fields").innerHTML = '';
+
+    renderTable();
 })
